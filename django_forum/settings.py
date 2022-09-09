990a49498a333cafd4b722a7d338a7b4f7086413
MiteshBTrivedi <mitesh@techis.io>
2022-09-10 04:56:34 +0530

    updated

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,14 +122,20 @@ cloudinary.config(
 )
 
 
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[   
-    BASE_DIR / 'static'
-]
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
